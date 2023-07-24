5ffa52a0d83b933b268ebd51353f0859a9c47bf0
Sivaram46 <t.s.ram46@gmail.com>
2023-07-25 00:21:55 +0530

    Configuring tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Layout } from "./components/Layout";
 
 function App() {
-  return <p>Hello world from App</p>;
+  return <Layout />;
 }
 
 export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Layout = () => {
+  return <div className="h-full bg-blue-500 m-5">Layout</div>;
+};
